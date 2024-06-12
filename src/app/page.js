ecd8b2a7f753756b827bfe1fa3c4e950a7784707
Justin Clarke <justinclarke241@gmail.com>
2024-06-12 22:21:35 +0100

    change

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
 				</div>
 			</header>
 
-			{/* Feature */}
+			{/* Features */}
 			<section className="text-gray-600 body-font">
 				<div className="container px-5 py-24 mx-auto">
 					<div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
